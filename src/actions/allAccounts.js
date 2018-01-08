978c4a30227243c5ac4c7ff60adbb81bff6c15d5
LVHieu8396 <le.hieu.lvh@gmail.com>
2018-01-08 20:57:47 +0700

    cập nhật giai diện admin (chưa xong)

--- src/actions/allAccounts.js
+++ src/actions/allAccounts.js
@@ -25,3 +25,27 @@ export const getAllAccounts = values => (dispatch, getState) => {
         }
     })
 }
+
+const requestAllAddresses = data => ({
+    type: 'REQUEST_ALL_ADDRESSES_SUCCESS',
+    data
+})
+
+export const getAllAddresses = values => (dispatch, getState) => {
+    axios({
+        ...AXIOS_CONFIG,
+        method: 'post',
+        data: '',
+        url: SERVER_URL + '/GetAllAddressSystem'
+    }).then(result => {
+        console.log(result.data);
+        dispatch(requestAllAddresses(result.data));
+    }).catch(error => {
+        if (error.response) {
+            console.log(error.response.data);
+            console.log(error.response.status);
+        } else if (error.request) {
+            console.log(error.request);
+        }
+    })
+}
